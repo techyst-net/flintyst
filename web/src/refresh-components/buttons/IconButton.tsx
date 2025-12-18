ea93891b974dd2cfdcd53a91589a96bc9720d856
roshan <zeshanshakil0@gmail.com>
2025-12-18 10:11:40 -0800

    fix: TextView tooltip z-index (#6842)

--- web/src/refresh-components/buttons/IconButton.tsx
+++ web/src/refresh-components/buttons/IconButton.tsx
@@ -304,7 +304,7 @@ export default function IconButton({
   const buttonElement = (
     <button
       className={cn(
-        "flex items-center justify-center h-fit w-fit group/IconButton",
+        "flex items-center justify-center h-fit w-fit group/IconButton z-tooltip",
         internal ? "p-1" : "p-2",
         disabled && "cursor-not-allowed",
         internal ? "rounded-08" : "rounded-12",
