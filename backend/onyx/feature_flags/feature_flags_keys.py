76bbdf6100c2369d8d6e29caeb1cd72bb1bbc1c3
roshan <zeshanshakil0@gmail.com>
2025-12-25 16:35:53 -0500

    chore: clean up unused feature flag (#7042)

--- backend/onyx/feature_flags/feature_flags_keys.py
+++ backend/onyx/feature_flags/feature_flags_keys.py
@@ -2,5 +2,3 @@
 Feature flag keys used throughout the application.
 Centralizes feature flag key definitions to avoid magic strings.
 """
-
-DISABLE_SIMPLE_AGENT_FRAMEWORK = "disable-simple-agent-framework"
