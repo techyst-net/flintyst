e8b9b5a0aaa6079d74106bb4914051a4c35d59e9
Wenxi <zeshanshakil0@gmail.com>
2026-02-18 19:45:40 -0800

    fix: init all celery tasks for autodiscovery (#8539)

--- backend/onyx/background/celery/tasks/pruning/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Celery tasks for connector pruning."""
-
-from onyx.background.celery.tasks.pruning.tasks import check_for_pruning  # noqa: F401
-from onyx.background.celery.tasks.pruning.tasks import (  # noqa: F401
-    connector_pruning_generator_task,
-)
-
-__all__ = ["check_for_pruning", "connector_pruning_generator_task"]
