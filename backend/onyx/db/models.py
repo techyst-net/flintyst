59cfc0bfe1e0ed3b8c4d761b81348bde0459797a
Wenxi <zeshanshakil0@gmail.com>
2026-03-10 15:18:39 -0700

    chore: add cache_okay to EncryptedJson (#9252)

--- backend/onyx/db/models.py
+++ backend/onyx/db/models.py
@@ -189,6 +189,9 @@ class EncryptedString(_EncryptedBase):
 
 
 class EncryptedJson(_EncryptedBase):
+    cache_ok = (
+        True  # have to re-declare because _is_json value is different from parent
+    )
     _is_json: bool = True
 
     def process_bind_param(
