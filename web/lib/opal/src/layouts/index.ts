0740cf878dec186c053e82c4d445d6ac9eef6e58
Nikolas Garza <zeshanshakil0@gmail.com>
2026-07-17 13:29:37 -0700

    feat(opal): add ConfirmationModalLayout (#12873)

--- web/lib/opal/src/layouts/index.ts
+++ web/lib/opal/src/layouts/index.ts
@@ -95,3 +95,9 @@ export {
   type ToastLevel,
   type ToastOptions,
 } from "@opal/layouts/toast/store";
+
+/* ConfirmationModalLayout */
+export {
+  ConfirmationModalLayout,
+  type ConfirmationModalProps,
+} from "@opal/layouts/modal/components";
