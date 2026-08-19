78197feca59021c5f1dfe56c751c57bb921bdf38
Justin Tahara <zeshanshakil0@gmail.com>
2026-08-19 19:34:31 +0000

    fix(terraform): relax the provider pin on modules that work with aws 6.x (#14080)

--- deployment/terraform/modules/aws/vpc/versions.tf
+++ deployment/terraform/modules/aws/vpc/versions.tf
@@ -3,7 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
+      # Pinned to 5.x: aws_flow_log.log_group_name was removed in provider 6.0.
+      # Migrate the flow log to log_destination before relaxing this.
       version = "~> 5.100"
     }
   }
